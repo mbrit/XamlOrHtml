f115e3ae32415cc7bd7b5a83e2545cb1d26a16da
ZeroInfinite <shirazadam@live.co.uk>
2014-03-20 18:29:30 +0000

    removed sorting and commented out code

--- XamlOrHtml/Program.cs
+++ XamlOrHtml/Program.cs
@@ -47,13 +47,6 @@ namespace XamlOrHtml
                     }
                 }
 
-                // render...
-                //StringBuilder builder = new StringBuilder();
-                //RenderResult(builder, "Microsoft", msXaml, msHtml, msUnknown);
-                //builder.Append("\r\n");
-                //RenderResult(builder, "Non-Microsoft", nonMsXaml, nonMsHtml, nonMsUnknown);
-                ////MessageBox.Show(builder.ToString());
-
                 // show...
                 Process.Start(temp);
             }
@@ -67,40 +60,6 @@ namespace XamlOrHtml
                     Console.ReadLine();
             }
         }
-
-        //private static void RenderResult(StringBuilder builder, string name, int xaml, int html, int directx)
-        //{
-        //    decimal total = xaml + html + directx;
-        //    decimal percentageXaml = 0M;
-        //    decimal percentageHtml = 0M;
-        //    decimal percentageUnknown = 0M;
-
-        //    if(total > 0)
-        //    {
-        //        percentageXaml = xaml / total;
-        //        percentageHtml = html / total;
-        //        percentageUnknown = directx / total;
-        //    }
-
-        //    // ok...
-        //    builder.Append(name);
-        //    builder.Append(" --> ");
-        //    builder.Append("XAML: ");
-        //    builder.Append(xaml);
-        //    builder.Append(" (");
-        //    builder.Append((percentageXaml * 100).ToString("n0"));
-        //    builder.Append("%), ");
-        //    builder.Append("HTML: ");
-        //    builder.Append(html);
-        //    builder.Append(" (");
-        //    builder.Append((percentageHtml * 100).ToString("n0"));
-        //    builder.Append("%), ");
-        //    builder.Append("DirectX: ");
-        //    builder.Append(directx);
-        //    builder.Append(" (");
-        //    builder.Append((percentageUnknown * 100).ToString("n0"));
-        //    builder.Append("%)");
-        //}
 
         private static IEnumerable<PackageInfo> WalkPackages(out string temp)
         {
@@ -123,10 +82,7 @@ namespace XamlOrHtml
                     }
             }
 
-            // sort...
-            packages.Sort(new DisplayNameComparer());
-
-            var pckgs = packages.Where(p => p.PackageRootFolder.Contains("\\WindowsApps\\") & p.PackageRootFolder.Contains("Microsoft.VCLibs") == false).OrderBy(p => p.Type);
+            var pckgs = packages.Where(p => p.PackageRootFolder.Contains("\\WindowsApps\\") & !p.PackageRootFolder.Contains("Microsoft.VCLibs")).OrderBy(p => p.Type);
 
             // csv...
             temp = Path.GetTempFileName() + ".csv";
@@ -143,14 +99,6 @@ namespace XamlOrHtml
 
             // return...
             return packages;
-        }
-
-        private class DisplayNameComparer : IComparer<PackageInfo>
-        {
-            public int Compare(PackageInfo x, PackageInfo y)
-            {
-                return String.Compare(x.DisplayName, y.DisplayName, StringComparison.OrdinalIgnoreCase);
-            }
         }
     }
 }
